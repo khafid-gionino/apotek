c8d9142a075f8bcd6921e33b01b26b9b6ac7b7b0
khafid-gionino <achmadkhafids75@gmail.com>
2021-07-19 10:32:33 +0700

    first commit

---
+++ application/views/home/beranda.php
@@ -0,0 +1,30 @@
+
+<div class="jumbotron text-center">
+  <div class="text-center">
+  <h1>KANGEAN APOTEK</h1>
+  <p>Pembelian obat terpercaya</p>
+  </div>
+  
+</div>
+
+<div class="container">
+  <h1 class="text-center">Dibuat Oleh</h1>
+<table class="table ">
+  <thead>
+    <tr>
+      <th scope="col">No.</th>
+      <th scope="col">Nama</th>
+      <th scope="col">NIM</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Ach.Khafid salim</td>
+      <td>06.2017.1.06762</td>
+    </tr>
+    
+  </tbody>
+</table>
+</div>
+
